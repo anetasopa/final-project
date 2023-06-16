8aecca32de4f2796732fa1c3fd2873e85c133393
Aneta <sopaaneta@gmail.com>
2023-06-16 12:18:53 +0200

    Add select to the profile page

--- app/profile/[userName]/ProfileForm.tsx
+++ app/profile/[userName]/ProfileForm.tsx
@@ -2,13 +2,26 @@
 
 import React, { useState } from 'react';
 import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 import styles from './ProfileForm.module.scss';
 
-const categories = [
+export interface CategoriesOption {
+  readonly value: string;
+  readonly label: string;
+}
+
+export const categoriesOption: readonly CategoriesOption[] = [
   { value: 'running', label: 'Running' },
-  { value: 'travel', label: 'Travel' },
-  { value: 'reading', label: 'Reading' },
+  { value: 'programming', label: 'Programming' },
+  { value: 'football', label: 'Football' },
+  { value: 'tarvel', label: 'Tarvel' },
+  { value: 'computer game', label: 'Computer game' },
+  { value: 'trip', label: 'Trip' },
+  { value: 'swimming', label: 'Swimming' },
+  { value: 'nature', label: 'Nature' },
 ];
+
+const animatedComponents = makeAnimated();
 
 export default function ProfileForm() {
   const [name, setName] = useState('');
@@ -19,6 +32,7 @@ export default function ProfileForm() {
     <form className={styles.form} onSubmit={(event) => event.preventDefault()}>
       <label htmlFor="name">Name</label>
       <input
+        type="text"
         id="name"
         value={name}
         onChange={(event) => setName(event.currentTarget.value)}
@@ -26,6 +40,8 @@ export default function ProfileForm() {
       />
       <label htmlFor="description">Description</label>
       <textarea
+        style={{ fontSize: '14px' }}
+        type="text"
         name="description"
         id="description"
         value={description}
@@ -34,11 +50,21 @@ export default function ProfileForm() {
       >
         Description
       </textarea>
+
       <Select
+        className={styles.select}
+        closeMenuOnSelect={false}
+        components={categoriesOption}
+        defaultValue={[categoriesOption[4], categoriesOption[5]]}
+        isMulti
+        options={categoriesOption}
+      />
+
+      {/* <Select
         className={styles.select}
         onChange={setSelectedOption}
         options={categories}
-      />
+      /> */}
       <button className={styles.buttonCreate}>Create</button>
     </form>
   );
