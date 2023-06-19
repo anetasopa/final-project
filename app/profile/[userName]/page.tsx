c4bbd0261887638c2e28d3cbacd86924924e8aa8
Aneta <sopaaneta@gmail.com>
2023-06-19 11:40:42 +0200

    Remove unnecessary code

--- app/profile/[userName]/page.tsx
+++ app/profile/[userName]/page.tsx
@@ -2,9 +2,8 @@ import { cookies } from 'next/headers';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import React from 'react';
-import { z } from 'zod';
 import { getCategories } from '../../../database/categories';
-import { getUserByToken, getUsersByUserName } from '../../../database/users';
+import { getUserBySessionToken } from '../../../database/users';
 import { Category } from '../../../migrations/1686916405-createTableCategories';
 import styles from './page.module.scss';
 import ProfileForm from './ProfileForm';
@@ -12,19 +11,27 @@ import ProfileForm from './ProfileForm';
 type Props = { params: { userName: string } };
 
 export default async function Profile({ params }: Props) {
-  // 1) get token from cookie
+  // const cookieStore = cookies();
+  // const token = cookieStore.get('sessionToken');
+  // const tokenValue = token?.value;
+  // if (!tokenValue) {
+  //   notFound();
+  // }
+
+  // const user = await getUserByToken(tokenValue);
+  // console.log({ user });
+
+  // if (!user) {
+  //   notFound();
+  // }
+
   const cookieStore = cookies();
-  const token = cookieStore.get('sessionToken');
-  const tokenValue = token?.value;
-  if (!tokenValue) {
-    notFound();
-  }
+  const sessionToken = cookieStore.get('sessionToken');
 
-  // 2) use function getUserByToken
-  const user = await getUserByToken(tokenValue);
-  console.log({ user });
+  const user = !sessionToken?.value
+    ? undefined
+    : await getUserBySessionToken(sessionToken.value);
 
-  // 3) If no user send 401
   if (!user) {
     notFound();
   }
