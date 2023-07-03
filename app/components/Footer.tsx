b4f0d5451016fb821a37450595f1ddc0f40a0ed2
Aneta <sopaaneta@gmail.com>
2023-07-04 00:19:21 +0200

    Add footer to the application

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,34 @@
+import Link from 'next/link';
+import { FaInstagram, FaTwitter } from 'react-icons/fa';
 import styles from './Footer.module.scss';
 
 export default function Footer() {
-  return <div className={styles.containerFooter}></div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.navigation}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/benefits">Benefits</Link>
+      </div>
+      <div className={styles.socialMediaLinks}>
+        <a
+          href="https://twitter.com/example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href="https://instagram.com/example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+      </div>
+      <div className={styles.attribution}>
+        &copy; 2023 Your App <span>ChatSync</span>
+      </div>
+    </footer>
+  );
 }
