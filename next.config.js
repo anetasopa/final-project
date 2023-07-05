d10c2af3cbdcef751cbd63189a587d8173fc8af3
Aneta <sopaaneta@gmail.com>
2023-07-05 22:51:02 +0200

    Deploy to vercel

--- next.config.js
+++ next.config.js
@@ -1,35 +1,35 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     typedRoutes: true,
-//     serverActions: true,
-//   },
-//   // reactStrictMode: true,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   images: {
-//     domains: ['res.cloudinary.com'],
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({});
-
-const nextConfig = withPWA({
-  dest: 'public',
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
+const nextConfig = {
   experimental: {
     typedRoutes: true,
     serverActions: true,
   },
-});
+  // reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({});
+
+// const nextConfig = withPWA({
+//   dest: 'public',
+//   images: {
+//     domains: ['res.cloudinary.com'],
+//   },
+//   experimental: {
+//     typedRoutes: true,
+//     serverActions: true,
+//   },
+// });
+
+// module.exports = nextConfig;
