2273de816a2f3ed405df3a9996e3e046ac6ec4b8
Aneta <sopaaneta@gmail.com>
2023-07-05 12:37:48 +0200

    Remove commented lines in the messages route

--- app/api/(auth)/messages/route.ts
+++ app/api/(auth)/messages/route.ts
@@ -14,37 +14,6 @@ export async function POST(
 ): Promise<NextResponse<SaveMessagesResponseBodyPost>> {
   const body = await request.json();
 
-  console.log({ body1234567: body });
-
-  // const cookieStore = cookies();
-  // const sessionToken = cookieStore.get('sessionToken');
-
-  // const creatorUserId = !sessionToken?.value
-  //   ? undefined
-  //   : await getUserBySessionToken(sessionToken.value);
-
-  // console.log({ creatorUserId1234567: creatorUserId });
-
-  // if (!creatorUserId) {
-  //   return NextResponse.json(
-  //     {
-  //       error: 'There is no such user!',
-  //     },
-  //     { status: 401 },
-  //   );
-  // }
-
-  // let creatorUser = await getUsersById(creatorUserId);
-
-  // if (!creatorUser) {
-  //   return NextResponse.json(
-  //     {
-  //       error: 'There is no such user!',
-  //     },
-  //     { status: 400 },
-  //   );
-  // }
-
   await saveMessages(body.inputMessage, body.userId, body.receiverId);
 
   return NextResponse.json({ message: 'Message is added!' });
