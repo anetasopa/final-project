b55a5074d65dc5860e0dcdcf67354abcff509de4
Aneta <sopaaneta@gmail.com>
2023-07-05 22:50:12 +0200

    Deploy to vercel

--- next.config.js
+++ next.config.js
@@ -1,19 +1,35 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     typedRoutes: true,
+//     serverActions: true,
+//   },
+//   // reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     domains: ['res.cloudinary.com'],
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({});
+
+const nextConfig = withPWA({
+  dest: 'public',
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
   experimental: {
     typedRoutes: true,
     serverActions: true,
   },
-  // reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-};
+});
 
 module.exports = nextConfig;
