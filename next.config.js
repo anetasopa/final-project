5e23f90f2c29edbedc0b50e3d53b81fc18d10fad
Aneta <sopaaneta@gmail.com>
2023-07-06 11:00:35 +0200

    Remove unnecessary code from config file

--- next.config.js
+++ next.config.js
@@ -17,19 +17,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const withPWA = require('next-pwa')({});
-
-// const nextConfig = withPWA({
-//   dest: 'public',
-//   images: {
-//     domains: ['res.cloudinary.com'],
-//   },
-//   experimental: {
-//     typedRoutes: true,
-//     serverActions: true,
-//   },
-// });
-
-// module.exports = nextConfig;
