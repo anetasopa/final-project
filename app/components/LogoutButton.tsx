d48d64c42f5aa4326318349857242b01184a79dc
Aneta <sopaaneta@gmail.com>
2023-07-09 14:01:28 +0200

    Fix logout redirect

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-// import { RiLogoutCircleRLine } from 'react-icons/ri';
 import styles from './LogoutButton.module.scss';
 
 type Props = {
@@ -9,27 +7,17 @@ type Props = {
 };
 
 export function LogoutButton(props: Props) {
-  const router = useRouter();
-
   return (
-    <form className={styles.form}>
+    <div className={styles.form}>
       <button
         className={styles.buttonLogout}
-        formAction={async () => {
-          router.push('/');
+        onClick={async () => {
           await props.logout();
-          router.refresh();
         }}
       >
         {' '}
         Logout
-        {/* <RiLogoutCircleRLine
-          style={{
-            marginRight: '30px',
-          }}
-          size="30px"
-        /> */}
       </button>
-    </form>
+    </div>
   );
 }
